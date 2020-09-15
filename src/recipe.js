2a6b8d4193850359535f459be40a11511148a67e
Godwill Barasa <iamgodwillb@gmail.com>
2020-09-15 14:42:47 +0300

    styling

--- src/recipe.js
+++ src/recipe.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Recipe = () => {
+const Recipe = ({title, calories,image, ingredients}) => {
     return ( 
         <div>
-            <h1>Title</h1>
-           <p>Calories</p>
-           <img src = "" alt =""/>
+            <h1>{title}</h1>
+            <p>{calories}</p>
+            <ol>
+                {ingredients.map(ingredient =>(
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
+
+            <img src = {image} alt =""/>
         </div>
         
 
